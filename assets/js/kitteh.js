6006be249f88cca9898ad5ac0332a5a7c113e980
Kathryn Cassidy <kcassidy@tchpc.tcd.ie>
2019-10-03 15:27:19 +0100

    Iz fixing the website of Mo!

---
+++ assets/js/kitteh.js
@@ -0,0 +1,9 @@
+function eyes() {
+    if ($("#kitteh").attr("src") == "assets/images/eyesopen.jpg") {
+        $("#kitteh").attr("src", "assets/images/eyesclosed.jpg");
+    } else {
+        $("#kitteh").attr("src", "assets/images/eyesopen.jpg");
+    } 
+}
+
+
